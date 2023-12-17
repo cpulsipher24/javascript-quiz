531f6a2365ff907cca41c4d268e69f4d597d7896
Cpulsipher24 <collintpulsipher@gmail.com>
2023-12-17 11:26:12 -0700

    Created more quiz questions

---
+++ Develop/script.js
@@ -0,0 +1,30 @@
+const quizQuestions = [
+    { question: "What is JavaScript?", 
+    choices: ["A programming language", "A type of coffee", "A car brand"], 
+    correctAnswer: 0 },
+    {
+        question: "What is the result of the following expression: 2 + 2 * 2?",
+        choices: ["4", "6", "8", "10"],
+        correctAnswer: 3
+    },
+    {
+        question: "Which of the following is a primitive data type in JavaScript?",
+        choices: ["Object", "String", "Array", "Function"],
+        correctAnswer: 1
+    },
+    {
+        question: "What does the 'DOM' stand for in web development?",
+        choices: ["Document Object Model", "Data Object Model", "Dynamic Object Model", "Digital Object Model"],
+        correctAnswer: 0
+    },
+    {
+        question: "Which method is used to add an element to the end of an array in JavaScript?",
+        choices: ["push()", "pop()", "shift()", "unshift()"],
+        correctAnswer: 0
+    },
+    {
+        question: "What does CSS stand for?",
+        choices: ["Counter Strike: Source", "Corrective Style Sheet", "Computer Style Sheet", "Cascading Style Sheet"],
+        correctAnswer: 3
+    }
+];
