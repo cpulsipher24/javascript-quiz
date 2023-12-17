83a1cdd0a3ade087ccd2fa5513a06e3b707ef62e
Cpulsipher24 <collintpulsipher@gmail.com>
2023-12-17 11:31:24 -0700

    Adjusted quiz questions

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,13 @@
 const quizQuestions = [
-    { question: "What is JavaScript?", 
-    choices: ["A programming language", "A type of coffee", "A car brand"], 
-    correctAnswer: 0 },
+    { 
+        question: "What is JavaScript?", 
+        choices: ["A programming language", "A type of coffee", "A car brand"], 
+        correctAnswer: 0 
+    },
     {
-        question: "What is the result of the following expression: 2 + 2 * 2?",
-        choices: ["4", "6", "8", "10"],
-        correctAnswer: 3
+        question: "Which of the following is a method used to add an element to the end of an array in JavaScript?",
+        choices: ["concat()", "slice()", "push()", "splice()"],
+        correctAnswer: 2
     },
     {
         question: "Which of the following is a primitive data type in JavaScript?",
@@ -13,18 +15,13 @@ const quizQuestions = [
         correctAnswer: 1
     },
     {
-        question: "What does the 'DOM' stand for in web development?",
-        choices: ["Document Object Model", "Data Object Model", "Dynamic Object Model", "Digital Object Model"],
-        correctAnswer: 0
+        question: "What is the purpose of the 'this' keyword in JavaScript?",
+        choices: ["To declare a variable", "To refer to the current object", "To create a loop", "To define a function"],
+        correctAnswer: 1
     },
     {
         question: "Which method is used to add an element to the end of an array in JavaScript?",
         choices: ["push()", "pop()", "shift()", "unshift()"],
         correctAnswer: 0
     },
-    {
-        question: "What does CSS stand for?",
-        choices: ["Counter Strike: Source", "Corrective Style Sheet", "Computer Style Sheet", "Cascading Style Sheet"],
-        correctAnswer: 3
-    }
 ];
